7529c10ebee4247638dac4e4eccb9f732f5b7c01
DanielCancellieri <danielcancancellieri98@gmail.com>
2021-12-28 14:44:11 -0300

    se agrega el proyecto

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+const toggleTheme = document.getElementById('toggle-theme');
+const toggleIcon = document.getElementById('toggle-icon');
+const toggleText = document.getElementById('toggle-text');
+
+const toggleColors = document.getElementById('toggle-colors');
+
+const rootstyles = document.documentElement.style;
+
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll(
+  "[data-section]");
+
+const chancgeLanguge = async language => {
+ const requestJson = await fetch(`./languges/${language}.json`);
+ const texts = await requestJson.json();
+
+for (const textToChange of textsToChange){
+ const section = textToChange.dataset.section;
+ const value = textToChange.dataset.value;
+
+  textToChange.innerHTML=texts[section][value];
+ 
+}
+}
+
+flagsElement.addEventListener("click", (e) =>{
+  chancgeLanguge(e.target.parentElement.dataset.language);
+})
+
+
+toggleTheme.addEventListener('click', ()=>{
+   document.body.classList.toggle('dark')
+   if(toggleIcon.src.includes('moon.svg')){
+       toggleIcon.src='assets/icons/sun.svg'
+       toggleText.textContent='Light mode'
+   } else{
+    toggleIcon.src='assets/icons/moon.svg'
+    toggleText.textContent='Dark mode'
+   }
+
+});
+
+toggleColors.addEventListener('click',(e)=>{
+  rootstyles.setProperty('--primary-color', e.target.dataset.color);
+
+});
